5e98afb6b899bd3bfe33b5f8b5196b0dc23fdd2d
rishavdhungel <rishavdhungel3@gmail.com>
2023-07-03 22:25:30 +0545

    playround implementation remaining

--- script.js
+++ script.js
@@ -1,75 +1,42 @@
-//This game is going to be played from console
+// Get random computer choice
+// Get player choice
 
-//player will play against computer
-//write getComputerChoice function to return random choices.
-//function that plays single round takes playerSelection and computerSelection and return reslut
-//make user input case insensitive
-//write function game() to play 5 round of game
+// playround
+//     check for conditions
+//     update screen values accordingly
 
-//getting computerSelection
-let choices = ["rock","paper","scissors"];
+//initializing variables
 let playerScore = 0;
 let computerScore = 0;
 let counter = 0;
 
-function getComputerChoice(){
+// Getting random computer choice
+const choices = ['rock','paper','scissors']
+let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+console.log(computerChoice);
 
-    computerChoice = choices[(Math.floor(Math.random()*choices.length))];
+//Getting player choice
+let  getplayer = function(){
+    let playerchoice = this.getAttribute('data-playerChoice');
+    console.log(playerchoice);
+  }
 
-    return computerChoice;
+const playerSelection = document.getElementsByClassName('choice');
+for (let i = 0; i<playerSelection.length;i++){
+
+    playerSelection[i].addEventListener('click',getplayer);
+
 }
-
-computerSelection = getComputerChoice();
-
-//getting player selection from console
 
 function playRound(){
 
-    let playerSelection = prompt("Pick your choice:rock/paper/scissors");
-    let lowerplayerSelection = playerSelection.toLowerCase();
+  
 
 
-    //check the winner of the game
-    //Rock beats scissors, scissors beat paper, and paper beats rock.
-    console.log(lowerplayerSelection);
-    console.log(computerSelection);
-    if(lowerplayerSelection==computerSelection){
-        console.log("Its a tie");
-        counter += 1;
 
-    }
-    else if((lowerplayerSelection=="rock" && computerSelection == "scissors")||(lowerplayerSelection=="scissors" && computerSelection == "paper")||(lowerplayerSelection=="paper" && computerSelection == "rock")){
-        console.log("Player Wins");
-        playerScore += 1;
-        counter += 1;
-        if (playerScore == 5){
-            alert("Player Wins the Game");
-            return;
-            
-        }
-    }else{
-        console.log("Computer Wins!");
-        computerScore += 1;
-        counter += 1;
-        if(computerScore == 5){
-            alert("Computer Wins the Game");
-            return;
-
-        }
-    }
 
 }
 
 
-while (counter<5){
-    playRound()
-    console.log(playerScore);
-    console.log(computerScore);
-}
 
 
-//initialize counter from zero 
-//repeat playround for 5 times
-//calculate result
-//greater score wins 
-//equal score tie
