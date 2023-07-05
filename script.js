3fc9029fcebc7e780a4ef8210cf821e702cdf163
rishavdhungel <rishavdhungel3@gmail.com>
2023-07-05 11:19:02 +0545

    PlayRound implemented.

--- script.js
+++ script.js
@@ -12,31 +12,67 @@ let counter = 0;
 
 // Getting random computer choice
 const choices = ['rock','paper','scissors']
-let computerChoice = choices[Math.floor(Math.random()*choices.length)];
-console.log(computerChoice);
+//add event listner to the three divs
+const Selection = document.querySelectorAll(".choice");
+console.log(Selection);
+let playerChoice;
 
-//Getting player choice
-let  getplayer = function(){
-    let playerchoice = this.getAttribute('data-playerChoice');
-    console.log(playerchoice);
-  }
+let getPlayer = function(){
+    playerChoice = this.getAttribute('data-playerChoice');
+    if (counter <5){
+        playRound()
+        
+    }else{
+        if(playerScore > computerScore){
+            alert("Player Wins the Game! Click Reset to play again!");
+            return;
+        }else if (computerScore > playerScore){
+            alert("Computer Wins the Game! Click Reset to play again!");
+            return;
+        }else{
+            alert("Its a tie! Click Reset to play again!");
+        } 
+    }
+    }
 
-const playerSelection = document.getElementsByClassName('choice');
-for (let i = 0; i<playerSelection.length;i++){
-
-    playerSelection[i].addEventListener('click',getplayer);
-
+for (let i = 0; i<Selection.length; i++){
+    Selection[i].addEventListener('click',getPlayer);
 }
 
 function playRound(){
+    console.log(playerChoice);    
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    console.log(computerChoice);
 
-  
+    if(playerChoice == computerChoice){
+        console.log("It's a tie");
+        counter += 1;
+        playerScore += 1;
+        console.log("Counter is:",counter);
+        console.log("Player Score",playerScore);
+        // check()
 
+    }
+    else if((playerChoice == "rock" && computerChoice == "scissors")||(playerChoice == "scissors" && computerChoice == "paper")||(playerChoice == "paper" && computerChoice == "rock")){
+        // console.log("player win!");
+        counter += 1;
+        console.log("Counter is:",counter);
+        console.log("computer Score",computerScore);
+        console.log("Player Score",playerScore);
+        // check()
+    
+    }
+    else{
+        // console.log('Computer Win');
+        counter += 1;
+        computerScore += 1;
+        console.log("Counter is:",counter);
+        console.log("computer Score",computerScore);
+        // check()
+    }
 
-
-
+    function check(){
+       
+    }
 }
-
-
-
 
